9a87234b5f55ac7e92fd6043c1719ea3a2ab5468
thejuly <dcsbailey21574@gmail.com>
2017-04-25 18:26:28 +0700

    ok

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = ''1sAimDh9L0wg5XAJeBYy96GrwzZFYQYAfTB+CrsdX5E/9gdt9oj5Lbc9NbRSHh5d05Ne9FDan12z6c9dbprv5TXRgjm4/+ioHTy6G91A2rNBiW999c0IlhsTcq5bfOyVf7OLRGtAAISIbHd84WemFgdB04t89/1O/w1cDnyilFU= '';
 
 // Get POST body content
 $content = file_get_contents('php://input');
