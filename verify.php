d5097cf38d0cf0eb5716f4355fd8d024c2d60e8c
thejuly <dcsbailey21574@gmail.com>
2017-04-25 19:48:22 +0700

    ok

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = '1sAimDh9L0wg5XAJeBYy96GrwzZFYQYAfTB+CrsdX5E/9gdt9oj5Lbc9NbRSHh5d05Ne9FDan12z6c9dbprv5TXRgjm4/+ioHTy6G91A2rNBiW999c0IlhsTcq5bfOyVf7OLRGtAAISIbHd84WemFgdB04t89/1O/w1cDnyilFU= ';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
